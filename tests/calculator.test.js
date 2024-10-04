2df9b3f2b470ac172001fa5684541dd245d179c4
Kevin Downing <36209619+v2@users.noreply.github.com>
2024-10-04 10:50:15 -0400

    remove display tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -6,10 +6,6 @@ describe('Calculator Memory Functions', function() {
     
 });
 
-// Test display operations (assuming appendToDisplay logic is inside calculator.js)
-describe('Calculator Display Functions', function() {
-    // TODO: appendToDisplay function
-});
 
 // Test calculation functions
 describe('Calculator Calculation Functions', function() {
